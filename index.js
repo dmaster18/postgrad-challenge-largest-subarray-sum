6b391c1bdea999eb3eec13621cd0212247fe49df
David <dmaster18@gmail.com>
2021-12-19 15:51:35 -0500

    deletion of description

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ function largestSubarraySum(integerArray) {
 }
 
 
-describe('#largestSubarraySum', function() {
-    it('returns the largest subarray sum for an array of all-positive integers', function () {
-          let array = [1, 100, 4, 15, 9, 30]
-          expect(largestSubarraySum(array)).toEqual(159)
-    });
-  
